92d43bdbd4171ac4e5519f2eac68eb9c54510ff7
Luis Miguel Aranguren Ardila <miguelo.5@hotmail.com>
2024-01-30 15:53:04 -0500

    en este commit guardamos cambios para ue funcionara la gema shoulda matcher la cual nos ayuda para las pruebas de modelo y comprobar ue se validen la presencia de campos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :address, presence: true
+    validates :living_place, presence: true
+    validates :observations, presence: true
+    validates :birthdate, presence: true
 end
