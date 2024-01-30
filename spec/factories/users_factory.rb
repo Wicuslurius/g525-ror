24cb401ad09e5d024d8f7c258e2054e0d01f46db
Luis Miguel Aranguren Ardila <miguelo.5@hotmail.com>
2024-01-30 14:06:21 -0500

    en este commit guardo la creacion de los accionables del controlador user , ademoas de modificar el archivo users_Factory

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
     name { 'miguel' }
+    last_name { 'Aranguren' }
+    email { 'email' }
+    address { 'car 41 a # 35 -32 sur' }
+    living_palce { 'casa' }
+    observations { 'desarrollador ruby on rails' }
+    birthdate { '1994-07-25' }
   end
 end
